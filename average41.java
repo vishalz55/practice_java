93a67a24c4615a476c37118598602262ccd41b9a
vishalz55 <166546124+vishalz55@users.noreply.github.com>
2024-09-06 22:40:11 +0530

    Add files via upload

---
+++ average41.java
@@ -0,0 +1,15 @@
+public class Average41 {
+
+
+    public static void main(String[] args) {
+        double[] numArray = { 45.3, 67.5, -45.6, 20.34, 33.0, 45.6 };
+        double sum = 0.0;
+
+        for (double num: numArray) {
+           sum += num;
+        }
+
+        double average = sum / numArray.length;
+        System.out.format("The average is: %.2f", average);
+    }
+}
